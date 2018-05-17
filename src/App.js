d2bbd258ef5d7396d75ca9e3ee71b32ccf6aa3c0
Anthony Wong <anthony.wong182@gmail.com>
2018-05-17 16:37:45 -0700

    sass/components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import './styles/theme.css'
+import Home from './components/Home'
+import Skills from './components/Skills'
+import Contact from './components/Contact'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <Switch>
+          <Route exact path="/" render={() => {
+            return <Home />
+          }} />
+          <Route path="/skills" render={() => {
+            return <Skills />
+          }} />
+          <Route path="/contact" render={() => {
+            return <Contact />
+          }} />
+        </Switch>
       </div>
     )
   }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Skills extends Component {
+  render() {
+    return (
+      <div>
+        <h1>check my nice skills bb</h1>
+      </div>
+    )
+  }
+}
